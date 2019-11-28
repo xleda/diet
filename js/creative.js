b5151f03b0de8afbfee429666a46fc16ea28e42d
Lateralus <michal.czerwien@gmail.com>
2019-11-28 08:54:01 +0100

    Carousel

--- js/creative.js
+++ js/creative.js
@@ -1,8 +1,8 @@
-(function($) {
+(function ($) {
     "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
+    $('a.page-scroll').bind('click', function (event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -17,27 +17,31 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
+    $('.navbar-collapse ul li a').click(function () {
         $('.navbar-toggle:visible').click();
     });
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         // Transition effect for navbar 
-        $(window).scroll(function() {
-          // checks if window is scrolled more than 500px, adds/removes solid class
-          if($(this).scrollTop() > 200) { 
-              $('.navbar-expand-lg').addClass('solid');
-          } else {
-              $('.navbar-expand-lg').removeClass('solid');
-          }
+        $(window).scroll(function () {
+            // checks if window is scrolled more than 500px, adds/removes solid class
+            if ($(this).scrollTop() > 200) {
+                $('.navbar-expand-lg').addClass('solid');
+            } else {
+                $('.navbar-expand-lg').removeClass('solid');
+            }
         });
-});
+    });
 
-    $(function() {
-		$('.pop').on('click', function() {
-			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
-			$('#imagemodal').modal('show');   
-		});		
+    $('.carousel').carousel({
+        interval: 4000
+    });
+
+    $(function () {
+        $('.pop').on('click', function () {
+            $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+            $('#imagemodal').modal('show');
+        });
     });
 
 })(jQuery); // End of use strict
